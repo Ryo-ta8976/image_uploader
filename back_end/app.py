5bba63d81fc51506e51fe7d91015a2d3e1a9ac0b
r.akiyama <ryota.akiyama110630@e-seikatsu.co.jp>
2021-01-17 11:06:45 +0900

    画像をfirebaseに保存/画像url取得

--- back_end/app.py
+++ back_end/app.py
@@ -2,6 +2,10 @@ from flask import Flask, request, make_response, send_from_directory
 import os
 from datetime import datetime
 import werkzeug
+import firebase_admin
+from firebase_admin import credentials, storage
+import io
+
 
 app = Flask(__name__)
 
@@ -9,16 +13,33 @@ app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
 UPLOAD_DIR = os.getenv('UPLOAD_DIR_PATH') # ./uploads
 
+# firebase認証
+cred = credentials.Certificate('./serviceAccountKey.json')
+firebase_admin.initialize_app(cred, {'storageBucket': 'image-uploader-6cb38.appspot.com'})
+
 
 @app.route('/upload_image', methods=['POST'])
 def upload_image():
+    print(request.files)
     if 'file' in request.files:
         return make_response(jsonify({'result':'file is empty.'}))
     file = request.files['image']
-    fileName = file.filename
-    saveFileName = datetime.now().strftime('%Y%m%d_%H%M%S_') + werkzeug.utils.secure_filename(fileName)
+    saveFileName = datetime.now().strftime('%Y%m%d_%H%M%S_') + werkzeug.utils.secure_filename(file.filename)
     file.save(os.path.join(UPLOAD_DIR, saveFileName))
-    return 'http://localhost:5000/uploads/' + saveFileName
+    print(type(file))
+
+    bucket = storage.bucket()
+    blob = bucket.blob(saveFileName)
+    if saveFileName.split('.')[1] == 'png':
+        content_type = 'image/png'
+    else:
+        content_type = 'image/jpg'
+
+    blob = bucket.blob(saveFileName)
+    with open(UPLOAD_DIR + '/' + saveFileName, 'rb') as f:
+        blob.upload_from_file(f, content_type=content_type)
+    blob.make_public()
+    return blob.public_url
 
 
 @app.errorhandler(werkzeug.exceptions.RequestEntityTooLarge)
