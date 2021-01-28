e739b69d3caf1db9acb548a0da5b558fed75b3c2
r.akiyama <ryota.akiyama110630@e-seikatsu.co.jp>
2021-01-28 21:21:14 +0900

    画像urlをmysqlに格納

--- back_end/server/app.py
+++ back_end/server/app.py
@@ -1,21 +1,37 @@
-from flask import Flask, request, make_response, send_from_directory
+from flask import Flask, request, make_response, send_from_directory, jsonify
 import os
 from datetime import datetime
 import werkzeug
+import io
 import firebase_admin
 from firebase_admin import credentials, storage
-import io
+from server.database import init_db
+import server.models
+from flask_sqlalchemy import SQLAlchemy
+from server.models.model import Image
 
 
-app = Flask(__name__)
-
-# データサイズの上限:1MB
-app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
-UPLOAD_DIR = os.getenv('UPLOAD_DIR_PATH') # ./uploads
-
+UPLOAD_DIR = os.getenv('UPLOAD_DIR_PATH') # uploads/
 # firebase認証
 cred = credentials.Certificate('./serviceAccountKey.json')
 firebase_admin.initialize_app(cred, {'storageBucket': 'image-uploader-6cb38.appspot.com'})
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # データサイズの上限:1MB
+    app.config['MAX_CONTENT_LENGTH'] = 1 * 1024 * 1024
+
+    # DB接続設定
+    app.config.from_object('server.config.Config')
+    init_db(app)
+
+    return app
+
+
+app = create_app()
+db = SQLAlchemy(app)
 
 
 @app.route('/upload_image', methods=['POST'])
@@ -24,32 +40,31 @@ def upload_image():
         return make_response(jsonify({'result':'file is empty.'}))
     file = request.files['image']
     saveFileName = datetime.now().strftime('%Y%m%d_%H%M%S_') + werkzeug.utils.secure_filename(file.filename)
+    print(saveFileName)
     file.save(os.path.join(UPLOAD_DIR, saveFileName))
 
+    # CloudStorageに
+    # 保存
     bucket = storage.bucket()
     blob = bucket.blob(saveFileName)
     if saveFileName.split('.')[1] == 'png':
         content_type = 'image/png'
     else:
         content_type = 'image/jpg'
-
     blob = bucket.blob(saveFileName)
     with open(UPLOAD_DIR + '/' + saveFileName, 'rb') as f:
         blob.upload_from_file(f, content_type=content_type)
     blob.make_public()
-    return blob.public_url
+    imageUrl = blob.public_url
+
+    # 画像urlをmysqlに保存
+    image = Image(saveFileName, imageUrl)
+    db.session.add(image)
+    db.session.commit()
+
+    return imageUrl
 
 
 @app.errorhandler(werkzeug.exceptions.RequestEntityTooLarge)
 def handle_over_max_file_size(error):
     return make_response(jsonify({'result':'file size is overed.'}))
-
-
-@app.route('/uploads/<filename>', methods=['GET'])
-def show_uploaded_file(filename):
-    print(os.getenv('UPLOAD_DIR_PATH'))
-    return send_from_directory(os.getenv('UPLOAD_DIR_PATH'), filename)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
---
+++ back_end/server/models/model.py
@@ -0,0 +1,16 @@
+# from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from server.database import db
+
+# Base = declarative_base()
+
+class Image(db.Model):
+  __tablename__ = 'images'
+
+  id = Column(Integer, primary_key=True)
+  image_name = Column(String(300), unique=True)
+  url = Column(String(300), unique=True)
+
+  def __init__(self, image_name, url):
+        self.image_name = image_name
+        self.url = url
