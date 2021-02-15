b022e88166c027147f4baad5affbb157d0379537
r.akiyama <ryota.akiyama110630@e-seikatsu.co.jp>
2021-02-15 16:39:39 +0900

    heroku設定

--- back_end/server/config.py
+++ back_end/server/config.py
@@ -4,21 +4,21 @@ class DevelopmentConfig:
 
     DEBUG = True
 
-    try:
-        from .local_config import *
-    except ImportError:
-        pass
+    # try:
+    #     from .local_config import *
+    # except ImportError:
+    #     pass
 
-    if not DEBUG:
+    # if not DEBUG:
 
-        SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8'.format(**{
-            'db_name': os.getenv('DB_NAME', os.environ['DB_NAME']),
-            'user': os.getenv('DB_USER', os.environ['DB_USERNAME']),
-            'password': os.getenv('DB_PASSWORD', os.environ['DB_PASSWORD']),
-            'host': os.getenv('DB_HOST', os.environ['DB_HOSTNAME']),
-        })
-        SQLALCHEMY_TRACK_MODIFICATIONS = False
-        SQLALCHEMY_ECHO = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8'.format(**{
+        'db_name': os.getenv('DB_NAME', os.environ['DB_NAME']),
+        'user': os.getenv('DB_USER', os.environ['DB_USERNAME']),
+        'password': os.getenv('DB_PASSWORD', os.environ['DB_PASSWORD']),
+        'host': os.getenv('DB_HOST', os.environ['DB_HOSTNAME']),
+    })
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
 
 
 Config = DevelopmentConfig
