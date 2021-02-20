84b5f6bccf090deea2df79d34245e875b0b78495
r.akiyama <ryota.akiyama110630@e-seikatsu.co.jp>
2021-02-20 21:28:27 +0900

    マイグレートpath変更

--- back_end/server/database.py
+++ back_end/server/database.py
@@ -3,7 +3,7 @@ from flask_migrate import Migrate
 import os
 
 db = SQLAlchemy()
-MIGRATION_DIR = os.path.join('server/models', 'migrations')
+MIGRATION_DIR = os.path.join('back_end/server/models', 'migrations')
 
 def init_db(app):
   db.init_app(app)
