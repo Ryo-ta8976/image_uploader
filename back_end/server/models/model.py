4541eb3339f464400799ed88e42fbc33c3e2876c
r.akiyama <ryota.akiyama110630@e-seikatsu.co.jp>
2021-02-21 18:02:35 +0900

    maxの変更

--- back_end/server/models/model.py
+++ back_end/server/models/model.py
@@ -8,8 +8,8 @@ class ImageStore(db.Model):
   __tablename__ = 'images'
 
   id = Column(Integer, primary_key=True)
-  image_name = Column(String(300), unique=True)
-  url = Column(String(300), unique=True)
+  image_name = Column(String(80), unique=True)
+  url = Column(String(120), unique=True)
 
   def __init__(self, image_name, url):
         self.image_name = image_name
