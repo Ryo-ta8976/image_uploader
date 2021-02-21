a5ee2d93cebc87d7a7a94be1952419022cfcb649
r.akiyama <ryota.akiyama110630@e-seikatsu.co.jp>
2021-02-21 17:50:11 +0900

    コメント削除

--- back_end/server/config.py
+++ back_end/server/config.py
@@ -4,13 +4,6 @@ class DevelopmentConfig:
 
     DEBUG = True
 
-    # try:
-    #     from .local_config import *
-    # except ImportError:
-    #     pass
-
-    # if not DEBUG:
-
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8'.format(**{
         'db_name': os.getenv('DB_NAME', os.environ['DB_NAME']),
         'user': os.getenv('DB_USER', os.environ['DB_USERNAME']),
